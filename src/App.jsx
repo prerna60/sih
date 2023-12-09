17e9fdc7d8dfb7726049ebb298c51bbb6806ae18
prerna60 <135650516+prerna60@users.noreply.github.com>
2023-12-09 21:44:15 +0530

    creating a card component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Card from "./components/Card";
+
+const App = () => {
+  return (
+    <div className=" flex items-center justify-center p-5 flex-wrap gap-2">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
+
+export default App;
